5b48d5fd05a04e0cd131b84e37b591b589cd5623
Artyom Fedenka <artyom.fedenka@transferwise.com>
2017-04-08 12:34:45 +0100

    star service

--- src/main/java/com/teambeer/starlingApi/StarlingService.java
+++ src/main/java/com/teambeer/starlingApi/StarlingService.java
@@ -1,8 +1,17 @@
 package com.teambeer.starlingApi;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by artyom.fedenka on 4/8/17.
  * ONWARDS!!!!
  */
+@Service
 public class StarlingService {
+
+	public List<TransactionsOut> latestTransactions() {
+		return null;
+	}
 }
