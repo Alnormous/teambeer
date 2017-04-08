cba37d332a50f5b66929393fc62b5fddd25eab2c
Dhinesh kumar <Dhinesh@Dhineshs-MacBook-Pro.local>
2017-04-08 12:30:50 +0100

    Starling

---
+++ src/main/java/com/teambeer/starlingApi/TransactionsOut.java
@@ -0,0 +1,58 @@
+package com.teambeer.starlingApi;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.LocalDateTime;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TransactionsOut {
+
+    public String merchantname;
+    public String merchantcode;
+    public int transactionamount;
+    public LocalDateTime transactiontime;
+
+    public TransactionsOut() {
+    }
+
+    public String getMerchantname() {
+        return merchantname;
+    }
+
+    public void setType(String Merchantname) {
+        this.merchantname = Merchantname;
+    }
+
+    public String getMerchantcode() {
+        return merchantcode;
+    }
+
+    public void setMerchantcode(String Merchantcode) {
+        this.merchantcode = Merchantcode;
+    }
+    
+    public LocalDateTime getTransactiontime() {
+        return transactiontime;
+    }
+
+    public void setTransactiontime(LocalDateTime Transactiontime) {
+        this.transactiontime = Transactiontime;
+    }
+    
+    public int getTransactionamount() {
+        return transactionamount;
+    }
+
+    public void setTransactionamount(int Transactionamount) {
+        this.transactionamount = Transactionamount;
+    }    
+    
+    @Override
+    public String toString() {
+        return "TransactionsOut{" +
+                "merchantname='" + merchantname + '\'' +
+                "merchantcode='" + merchantcode + '\'' +
+                "transactionamount='" + transactionamount + '\'' +
+                ", transactiontime=" + transactiontime +
+                '}';
+    }
+}
