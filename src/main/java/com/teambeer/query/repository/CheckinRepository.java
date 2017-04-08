f27486a47e06b0713c1c76390ba4b3b3638544a7
Alister Stielow <alister.stielow@hivehome.com>
2017-04-08 21:52:50 +0100

    scheduled untappd task

---
+++ src/main/java/com/teambeer/query/repository/CheckinRepository.java
@@ -0,0 +1,52 @@
+package com.teambeer.query.repository;
+
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.teambeer.untappd.model.Item;
+
+@Service
+public class CheckinRepository {
+	
+	private static final String UNTAPPD_CHECKINS = "checkins";
+
+	@Autowired
+	private HazelcastInstance hazelcast;
+
+	private IMap<Long, Item> checkins;
+
+	@PostConstruct
+	public void init() {
+		checkins = hazelcast.getMap(UNTAPPD_CHECKINS);
+	}
+
+	public boolean storeCheckin(Item checkin) {
+		if (checkins.containsKey(checkin.getCheckinId())) {
+			storeCheckin(checkin);
+			return true;
+		}
+		return false;
+	}
+
+	public Item getByCheckin(Item checkin) {
+		return checkins.get(checkin.getCheckinId());
+	}
+
+	public Collection<Item> getAll() {
+		return checkins.values();
+	}
+
+	public void removeCheckin(Item checkin) {
+		checkins.remove(checkin.getCheckinId());
+	}
+	
+	public void updateCheckin(Item checkin) {
+		checkins.replace(checkin.getCheckinId(), checkin);
+	}
+}
---
+++ src/main/java/com/teambeer/ruleengine/UntappdScheduledTask.java
@@ -0,0 +1,47 @@
+package com.teambeer.ruleengine;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.teambeer.query.repository.CheckinRepository;
+import com.teambeer.untappd.UntappdController;
+import com.teambeer.untappd.UserNotFoundException;
+
+@Component
+@EnableScheduling
+public class UntappdScheduledTask {
+
+	@Value("${untappd.user}")
+	private String user;
+
+	@Autowired
+	private UntappdController untappd;
+	
+	@Autowired
+	private CheckinRepository checkinRepo;
+	
+	@Autowired
+	private MatcherEngine matcherEngine;
+
+	private Logger log = LoggerFactory.getLogger(UntappdScheduledTask.class);
+
+	@Scheduled(cron = "*/45 * * * * *")
+	public void getUntappdCheckins() throws UserNotFoundException {
+		log.info("Getting checkins from Untappd for user {}", user);
+		untappd.getCheckinsByUser(user).forEach(item -> {
+			if (checkinRepo.storeCheckin(item)) {
+				final LocalDateTime dateTime = LocalDateTime.parse(item.getCreatedAt(), DateTimeFormatter.RFC_1123_DATE_TIME);
+				matcherEngine.analyzeBeer(item.getBeer().getBeerName(), dateTime, null);
+			}
+		});
+	}
+
+}
