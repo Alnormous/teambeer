58133ea08de27d44d3d659d55a4d597fc3a1199c
anorman <andrew.norman@gmail.com>
2017-04-08 10:04:00 +0100

    Initial check in

---
+++ src/main/java/com/teambeer/BeeranalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.teambeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeranalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeranalyzerApplication.class, args);
+	}
+}
