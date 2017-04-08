4f8afb98b9174ae863b9b00083d37d1f99369ffa
Artyom Fedenka <artyom.fedenka@transferwise.com>
2017-04-08 10:30:23 +0100

    Matcher engine api

---
+++ src/main/java/com/teambeer/ruleengine/MatcherEngine.java
@@ -0,0 +1,15 @@
+package com.teambeer.ruleengine;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by artyom.fedenka on 4/8/17.
+ * ONWARDS!!!!
+ */
+public class MatcherEngine {
+
+	public BigDecimal analyzeBeer(String beerName) {
+		return null;
+	}
+
+}
