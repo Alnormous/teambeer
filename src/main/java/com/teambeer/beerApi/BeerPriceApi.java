c9083dc7eea033425002248ab1186dc56efa5476
Artyom Fedenka <artyom.fedenka@transferwise.com>
2017-04-08 10:37:51 +0100

    Added beer api interface

---
+++ src/main/java/com/teambeer/beerApi/BeerPriceApi.java
@@ -0,0 +1,14 @@
+package com.teambeer.beerApi;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by artyom.fedenka on 4/8/17.
+ * ONWARDS!!!!
+ */
+public class BeerPriceApi {
+
+	public BigDecimal findBeerPrice(String query) {
+		return null;
+	}
+}
